581fe093683037834ee5d09359b6d6dfc165f1e5
borsmar <borsmar123@gmail.com>
2021-06-15 01:51:45 +0300

    Initial commit with most of deps

---
+++ src/main/java/com/example/clickandcounter/ClickandcountApplication.java
@@ -0,0 +1,13 @@
+package com.example.clickandcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickandcountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickandcountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clickandcounter/ClickandcountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clickandcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickandcountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
